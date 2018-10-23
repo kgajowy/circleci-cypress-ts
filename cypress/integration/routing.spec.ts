2865f1e516569e077726db1d4aaf0d253058aa7c
kgajowy <kamil.gajowy@9yards.at>
2018-10-23 14:48:47 +0200

    chore: sample

--- cypress/integration/routing.spec.ts
+++ cypress/integration/routing.spec.ts
@@ -12,7 +12,20 @@
 
 describe('Main Navigation', () => {
     it('Contains navigation entries', () => {
-        cy.visit('localhost:3000')
-        cy.contains(/To get started/)
+        cy
+            .setCookie('gdpr_permission_given', '1')
+            .visit('https://allegro.pl/kategoria/pokoj-dzieciecy-meble-17273?order=m')
+            .get('article[data-item=true]')
+            .first()
+            .click()
+            .get('#add-to-cart-button')
+            .click()
+            .get('#add-to-cart-si-precart')
+            .click()
+            .get('.m-input')
+            .type('0')
+            .get('.cart-quantity').should('have.value', '10')
+
+
     })
 })
